317f5e63cba86ef7a698c39a975bdc7ce0c043cb
Tom Spurling <tom@tspurling.co.uk>
2020-02-06 08:38:45 +0000

    fixup! Use worktree by default

--- build-container.sh
+++ build-container.sh
@@ -1,2 +1,9 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+docker() {
+	command docker "$@" || (>&2 echo "trying with sudo..." && sudo docker "$@")
+}
+
+[[ -d src ]] || git worktree add src redo
+
 docker build -t mitxela/pict -f container/Dockerfile .
