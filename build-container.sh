2c45d77fd91a0d2c05fd22b2b4237c3ffb158c19
Tom Spurling <tom@tspurling.co.uk>
2020-02-21 23:33:00 +0000

    Unclean build warning!

--- build-container.sh
+++ build-container.sh
@@ -5,4 +5,9 @@
 # Config to match container db's default credentials
 cp container/_db.php src/_db.php
 
+(cd src; git clean -nxd | grep -v _db.php -q) && (
+	>&2 echo 'WARNING -- src directory is not clean, do not push image!'
+	>&2 echo
+)
+
 docker build -t mitxela/pict -f container/Dockerfile .
