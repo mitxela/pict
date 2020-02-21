1d8329e76da363ddd3e056ea6a1ca5b6a53a0111
Tom Spurling <tom@tspurling.co.uk>
2020-02-21 23:17:54 +0000

    _db.php is mandatory for container build

--- build-container.sh
+++ build-container.sh
@@ -2,4 +2,7 @@
 
 [[ -d src ]] || git worktree add src master
 
+# Config to match container db's default credentials
+cp container/_db.php src/_db.php
+
 docker build -t mitxela/pict -f container/Dockerfile .
